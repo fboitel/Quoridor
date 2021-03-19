60b13085c680d059674a0157de0b5df519807a7c
SACCOCCIO Clement <csaccoccio@enseirb-matmeca.fr>
2021-03-19 17:42:21 +0100

    added error messages

--- src/server.c
+++ src/server.c
@@ -1,101 +1,128 @@
-#include "player.h"         // temporaly
-#include <getopt.h>         // for option
 #include <gsl/gsl_matrix.h> // for matrix
-#include <string.h> // for strcmp
-#include <unistd.h> // for check access to file
+#include <stdlib.h>
+#include <string.h>         // for strcmp
+#include <unistd.h>         // to check file existence
+#include "player.h"         // temporary
 
 
-enum board_shape_t {SQUARE, TORIC, H, SNAKE, NOT_SHAPE = -1};
+enum board_shape_t {
+	SQUARE, TORIC, H, SNAKE, INVALID_SHAPE = -1
+};
 
-int boardSize = 10;
+int board_size = 10;
 enum board_shape_t boardShape = SQUARE;
-char * pathPlayer1 = NULL;
-char * pathPlayer2 = NULL;
+char *player_1_path = NULL;
+char *player_2_path = NULL;
 
-enum board_shape_t parseBoardShape(char * t) {
-	if ( strlen(t) > 1) {
-		return NOT_SHAPE;
+enum board_shape_t parse_board_shape(char *t) {
+	if (strlen(t) != 1) {
+		return INVALID_SHAPE;
 	}
+
 	switch (*t) {
-		case 'c' :
-		return SQUARE;
-		
-		case 't' :
-		return TORIC;
-		
-		case 'h' :
-		return H;
+		case 'c':
+			return SQUARE;
 
-		case 's' :
-		return SNAKE;
+		case 't':
+			return TORIC;
 
-		default :
-			return NOT_SHAPE;
+		case 'h':
+			return H;
+
+		case 's':
+			return SNAKE;
+
+		default:
+			return INVALID_SHAPE;
 	}
 }
 
-void parse_opts(int argc, char *argv[]) {
+void usage(char* exec_path, char *message) {
+	if (message != NULL) {
+		fprintf(stderr, "%s\n\n", message);
+	}
+	fprintf(stderr, "Usage: %s [-m SIZE] [-t SHAPE] <PLAYER_1_PATH> <PLAYER_2_PATH>\n", exec_path);
+}
 
-	int nbPlayers = 0;
-
+void parse_args(int argc, char **argv) {
 	for (int i = 1; i < argc; ++i) {
-  //  	printf("%s \n", argv[i]);
 		char *arg = argv[i];
 
-
-// TODO check if it's a number
-		if( !strcmp(arg, "-m")) {
+		if (!strcmp(arg, "-m")) {
 			++i;
 
-			if ( i >= argc || (boardSize = atoi(argv[i])) <= 0 ) { // check size
-			printf("USAGE1\n");
-			exit(1);
+			if (i == argc) {
+				usage(argv[0], "\"-m\" option must be followed by the board size.");
+				exit(EXIT_FAILURE);
 			}
-		} else if( !strcmp(arg, "-t")) {
+
+			if ((board_size = atoi(argv[i])) <= 0) {
+				usage(argv[0], "Board size must be a strictly positive number.");
+				exit(EXIT_FAILURE);
+			}
+
+		} else if (!strcmp(arg, "-t")) {
 			++i;
-			if ( i >= argc || (boardShape = parseBoardShape(argv[i])) == NOT_SHAPE) { // check c t h or s
-				printf("USAGE2\n");
-				exit(1);
+
+			if (i == argc) {
+				usage(argv[0], "\"-t\" option must be followed by the board shape.");
+				exit(EXIT_FAILURE);
 			}
+
+			if ((boardShape = parse_board_shape(argv[i])) == INVALID_SHAPE) {
+				usage(argv[0], "Board shape must be \"c\", \"t\", \"h\" or \"s\".");
+				exit(EXIT_FAILURE);
+			}
+
 		} else {
-			if (pathPlayer2 != NULL || access( argv[i], F_OK)) {
-				printf("USAGE3\n");
-				exit(1);
-			}
-			if ( pathPlayer1 == NULL) {
-				pathPlayer1 = argv[i];
-			} else {
-				pathPlayer2 = argv[i];
+			if (player_2_path != NULL) {
+				printf(argv[0], "There is too much players.");
+				exit(EXIT_FAILURE);
 			}
 
-		}
-    }
-	if (pathPlayer2 == NULL) {
-				printf("USAGE4\n");
-				exit(1);
+			if (access(argv[i], F_OK)) {
+				char *message_start = "File \"";
+				char *message_end = "\" doesn't exists.";
+				char message[strlen(message_start) + strlen(argv[i]) + strlen(message_end) + 1];
+				sprintf(message, "%s%s%s", message_start, argv[i], message_end);
+				usage(argv[0], message);
+				exit(EXIT_FAILURE);
 			}
+
+			if (player_1_path == NULL) {
+				player_1_path = argv[i];
+			} else {
+				player_2_path = argv[i];
+			}
+		}
+	}
+
+	if (player_2_path == NULL) {
+		usage(argv[0], "Not enough players.");
+		exit(EXIT_FAILURE);
+	}
 }
 
 int main(int argc, char *argv[]) {
 
-  parse_opts(argc, argv);
+	parse_args(argc, argv);
 
-  // init a new matrix size of 3x3
-  gsl_matrix *board = gsl_matrix_alloc(3, 3);
+	// init a new matrix size of 3x3
+	gsl_matrix *board = gsl_matrix_alloc(3, 3);
 
-  // free the matrix
-  gsl_matrix_free(board);
+	// free the matrix
+	gsl_matrix_free(board);
 
-  /* pseudo code for game loop
-  start_player = random()
-  for each player p
-      p->initialize(p == start_player ? BLACK : WHITE, graph, num_walls)
-  while true
-     p = compute_next_player()
-     move = p->play(move)
-     if is_winning()
-        break
-  for each player p
-     p->finalize()
-     */
+	/* pseudo code for game loop
+	start_player = random()
+	for each player p
+		p->initialize(p == start_player ? BLACK : WHITE, graph, num_walls)
+	while true
+	   p = compute_next_player()
+	   move = p->play(move)
+	   if is_winning()
+		  break
+	for each player p
+	   p->finalize()
+	   */
 }
