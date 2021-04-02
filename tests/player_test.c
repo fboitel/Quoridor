907e18072634bf7c355337cd8663a555c28c2f1a
GOMICHON Theo <Theo.Gomichon@bordeaux-inp.fr>
2021-04-02 18:34:14 +0200

    Move test macros in test header

--- tests/player_test.c
+++ tests/player_test.c
@@ -9,12 +9,6 @@
 #include <string.h>
 #include <assert.h>
 
-int passed = 0;
-int total = 0;
-int pass;
-
-#define TEST(f) setup(); f(); teardown(); printf("\t%s\n", pass ? "OK" : "FAIL"); passed += pass; total++;
-
 extern struct player_t player;
 extern char *name;
 
@@ -22,46 +16,36 @@ size_t n = 3;
 size_t edges = 0;
 struct graph_t *board = NULL;
 
-
-void setup(void) {
-    pass = 1;
-
+static void setup(void) {
     n = 3;
     board = graph_init(n, SQUARE);
     edges = n * n * (n - 1) * (n - 1) * 2;
     initialize(BLACK, board, ceil(edges / 15));
 }
 
-void teardown(void) {
+static void teardown(void) {
     graph_free(player.graph);
     finalize();
-}
-
-void fail(char* msg) {
-    fprintf(stderr, "ERROR: %s\n", msg);
-    pass = 0;
 }
 
 void test_initialization(void) {
     printf("%s", __func__);
 
-    if (player.id != BLACK) {
-        fail("Id initialization failed");
-    }
-    if (player.num_walls != ceil(edges / 15)) {
-        fail("Wrong number of walls initialized");
-    };
+    if (player.id != BLACK)
+        FAIL("Id initialization failed");
+
+    if (player.num_walls != ceil(edges / 15))
+        FAIL("Wrong number of walls initialized");
+
     // TODO Check player.graph
 }
 
 void test_get_player_name(void) {
     printf("%s", __func__);
 
-    if(strcmp(get_player_name(), name)) {
-        fail("Can not get player name");
-    };
+    if(strcmp(get_player_name(), name))
+        FAIL("Can not get player name");
 }
-
 
 void test_play_random(void) {
     printf("%s", __func__);
@@ -71,6 +55,6 @@ void test_player_main(void) {
     TEST(test_initialization);
     TEST(test_get_player_name);
     // TEST(test_play_random);
-    
-    printf("%s (%d/%d) --> %s\n", __func__, passed, total, passed == total ? "PASSED" : "FAILED");
+
+    printf("%s (%d/%d) --> %s\n\n", __func__, passed, total, passed == total ? "PASSED" : "FAILED");
 }
--- tests/tests.h
+++ tests/tests.h
@@ -1,6 +1,13 @@
 #ifndef _QUOR_TESTS_H_
 #define _QUOR_TESTS_H_
 
+// Test macros
+static int pass;
+static int passed = 0;
+static int total = 0;
+#define TEST(f) pass = 1, setup(), f(), teardown(), printf("\t%s\n", pass ? "OK" : "FAIL"), passed += pass, total++
+#define FAIL(msg) fprintf(stderr, "ERROR: %s\n", msg), pass = 0
+
 void test_player_main(void);
 
 #endif // _QUOR_TESTS_H_
