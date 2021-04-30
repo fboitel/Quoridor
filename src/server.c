53d67d7c9d67d6286371b03c58044c76477d72bc
LANGLAIS Hugo <Hugo.Langlais@bordeaux-inp.fr>
2021-04-30 15:03:11 +0200

    refracto server.c

--- src/server.c
+++ src/server.c
@@ -11,6 +11,12 @@ extern char* player_1_path;
 extern char* player_2_path;
 extern int board_size;
 
+bool game_over = false;
+size_t position_player_1 = -1;
+size_t position_player_2 = -1;
+enum color_t active_player = -1;
+enum color_t winner = -1;
+
 void* P1_lib;
 void (*P1_initialize)(enum color_t id, struct graph_t* graph, size_t num_walls);
 char* (*P1_name)(void);
@@ -21,7 +27,6 @@ void* P2_lib;
 void (*P2_initialize)(enum color_t id, struct graph_t* graph, size_t num_walls);
 char* (*P2_name)(void);
 struct move_t(*P2_play)(struct move_t previous_move);
-
 void (*P2_finalize)();
 
 // Open players libs
@@ -76,34 +81,40 @@ bool is_valid_wall(struct graph_t* board, struct edge_t e[]) {
 			vertex_from_direction(board, e[0].fr, EAST) == e[0].to &&
 			vertex_from_direction(board, e[1].fr, EAST) == e[1].to &&
 			(vertex_from_direction(board, e[0].fr, SOUTH) == e[1].fr ||
-			vertex_from_direction(board, e[0].to, SOUTH) == e[1].to)
+				vertex_from_direction(board, e[0].to, SOUTH) == e[1].to)
 			) || (
 				// horizontal wall
 				vertex_from_direction(board, e[0].fr, SOUTH) == e[0].to &&
 				vertex_from_direction(board, e[1].fr, SOUTH) == e[1].to &&
 				(vertex_from_direction(board, e[0].fr, EAST) == e[1].fr ||
-				vertex_from_direction(board, e[0].to, EAST) == e[1].to)
+					vertex_from_direction(board, e[0].to, EAST) == e[1].to)
 				);
 }
 
 // Check move validity
-bool check(struct move_t mv, struct graph_t* board, enum color_t player) {
+bool move_is_valid(struct move_t* mv, struct graph_t* board, enum color_t player) {
 	// Check type
-	if (mv.t < 0 || mv.t >= NO_TYPE) {
-		fprintf(stderr, "Error from %s: incorrect move type (%u)\n", player == BLACK ? P1_name() : P2_name(), mv.t);
+	if (mv->t < 0 || mv->t >= NO_TYPE) {
+		fprintf(stderr, "Error from %s: incorrect move type (%u)\n", player == BLACK ? P1_name() : P2_name(), mv->t);
+		winner = get_next_player(active_player);
+		game_over = true;
 		return false;
 	}
 
 	// Check color
-	if (mv.c != player) {
-		fprintf(stderr, "Error from %s: incorrect color (%u)\n", player == BLACK ? P1_name() : P2_name(), mv.c);
+	if (mv->c != player) {
+		fprintf(stderr, "Error from %s: incorrect color (%u)\n", player == BLACK ? P1_name() : P2_name(), mv->c);
+		winner = get_next_player(active_player);
+		game_over = true;
 		return false;
 	}
 
 	// Check if edges create a wall
-	if (mv.t == WALL) {
-		if (!is_valid_wall(board, mv.e)) {
-			fprintf(stderr, "Error from %s: bad wall (%zu, %zu), (%zu, %zu)\n", player == BLACK ? P1_name() : P2_name(), mv.e[0].fr, mv.e[0].to, mv.e[1].fr, mv.e[1].to);
+	if (mv->t == WALL) {
+		if (!is_valid_wall(board, mv->e)) {
+			fprintf(stderr, "Error from %s: bad wall (%zu, %zu), (%zu, %zu)\n", player == BLACK ? P1_name() : P2_name(), mv->e[0].fr, mv->e[0].to, mv->e[1].fr, mv->e[1].to);
+			winner = get_next_player(active_player);
+			game_over = true;
 			return false;
 		}
 	}
@@ -112,6 +123,35 @@ bool check(struct move_t mv, struct graph_t* board, enum color_t player) {
 	return true;
 }
 
+void update_board(struct graph_t* board, struct move_t* last_move) {
+
+	// Update players positions if they move
+	if (last_move->t == MOVE) {
+		if (active_player == BLACK) {
+			position_player_1 = last_move->m;
+		}
+		else {
+			position_player_2 = last_move->m;
+		}
+	}
+	else {
+		// Update the board if a player has put a wall
+		placeWall(board, last_move->e);
+	}
+}
+
+void end_game() {
+	winner = active_player;
+	game_over = true;
+}
+
+void close_server(struct graph_t* board) {
+	P1_finalize();
+	P2_finalize();
+	graph_free(board);
+	dlclose(P1_lib);
+	dlclose(P2_lib);
+}
 
 int main(int argc, char* argv[]) {
 	// Parse arguments
@@ -126,7 +166,7 @@ int main(int argc, char* argv[]) {
 	printf("Libs loaded\n");
 
 	// Initialize a new board of size m and shape t
-	// TODO : init a new board depending on parameters
+	// TODO : init a new board depending on parameters + copy
 	size_t m = board_size;
 	struct graph_t* board = graph_init(m, SQUARE);
 	struct graph_t* boardCopy1 = graph_init(m, SQUARE);
@@ -137,8 +177,7 @@ int main(int argc, char* argv[]) {
 	int num_walls = 10;
 
 	// Init random starting player
-	enum color_t active_player = rand() % 2;
-	enum color_t winner;
+	active_player = rand() % 2;
 
 	// init players
 	P1_initialize(BLACK, boardCopy1, num_walls);
@@ -149,83 +188,46 @@ int main(int argc, char* argv[]) {
 	printf("%s begins\n", active_player == BLACK ? P1_name() : P2_name());
 
 	// Initialize the first move as a move to the intial place
-	struct move_t last_move = {
+	struct move_t last_move = (struct move_t){
 			.m = SIZE_MAX,
 			.c = active_player,
 			.e = {no_edge(), no_edge()},
 			.t = NO_TYPE
 	};
 
-	/*
-	// display adj matrix
-	for (size_t i = 0; i < m * m; ++i) {
-		for (size_t j = 0; j < m * m; ++j) {
-			printf("% d ", gsl_spmatrix_uint_get(board->t, i, j));
-		}
-		printf("\n");
-	}*/
-
-	size_t position_player_1 = -1;
-	size_t position_player_2 = -1;
-
 	// Game loop
-	bool game_over = false;
 	size_t turn = 0;
 
 	while (!game_over) {
 		turn++;
 
-		// Play
+		// Plays the active player
 		last_move = active_player == BLACK ? P1_play(last_move) : P2_play(last_move);
 
 		// Check move validity
-		if (!check(last_move, board, active_player)) {
-			printf("Invalid move from %s\n", active_player == BLACK ? P1_name() : P2_name());
-			winner = get_next_player(active_player);
+		if (!move_is_valid(&last_move, board, active_player)) {
 			break;
 		}
 
-		// update players positions
-		if (last_move.t == MOVE) {
-			if (active_player == BLACK) {
-				position_player_1 = last_move.m;
-			}
-			else {
-				position_player_2 = last_move.m;
-			}
-		}
-		else {
-			// Update the board if a player has put a wall
-			placeWall(board, last_move.e);
-		}
+		update_board(board, &last_move);
 
 		display_board(board, m, position_player_1, position_player_2);
-	//	printf("wall : %ld:%ld - %ld:%ld \n", last_move.e[0].fr,  last_move.e[0].to,  last_move.e[1].fr,  last_move.e[1].to);
+		//	printf("wall : %ld:%ld - %ld:%ld \n", last_move.e[0].fr,  last_move.e[0].to,  last_move.e[1].fr,  last_move.e[1].to);
 
 		// Check if a player has won
 		if (is_winning(board, active_player, active_player == BLACK ? position_player_1 : position_player_2)) {
-			winner = active_player;
-			game_over = true;
+			end_game();
+			break;
 		}
-		else {
-			active_player = get_next_player(active_player);
-		}
-		// display adj matrix
-	/*	for (size_t i = 0; i < m * m; ++i) {
-			for (size_t j = 0; j < m * m; ++j) {
-				printf("% d ", gsl_spmatrix_uint_get(board->t, i, j));
-			}
-			printf("\n");
-		}*/
+		
+		active_player = get_next_player(active_player);
 	}
+
 	printf("GAME OVER\n");
 	printf("%s won\n", winner == BLACK ? P1_name() : P2_name());
 	printf("Finish after %zu turns\n", turn);
-	P1_finalize();
-	P2_finalize();
-	graph_free(board);
-	dlclose(P1_lib);
-	dlclose(P2_lib);
+
+	close_server(board);
 
 	return EXIT_SUCCESS;
 }
