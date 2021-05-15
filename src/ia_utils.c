602ab984dade8785d87178bc950454dfdd39d60b
BOITEL Faustin <faustin.boitel@bordeaux-inp.fr>
2021-05-15 11:43:56 +0200

    modified make_first_move to be able to begin on serpent grah

--- src/ia_utils.c
+++ src/ia_utils.c
@@ -1,11 +1,15 @@
 #include "ia_utils.h"
 
 struct move_t make_default_first_move(struct game_state_t game) {
+	size_t vertex_owned = 0;
+	for (size_t i = 0; i < game.graph->num_vertices; i++)
+		if (gsl_spmatrix_uint_get(game.graph->o, i, 0))
+			vertex_owned++;
 	struct move_t move = {
 			.e = {no_edge(), no_edge()},
 			.t = MOVE,
 			.c = game.self.color,
-			.m = gsl_spmatrix_uint_get(game.graph->o, game.self.color, 0) ? (size_t)((sqrtl(game.graph->num_vertices)-1)/2) : game.graph->num_vertices - (size_t)(sqrtl(game.graph->num_vertices)/2)
+			.m = gsl_spmatrix_uint_get(game.graph->o, game.self.color, 0) ? (size_t)vertex_owned/2 : game.graph->num_vertices - (size_t)(vertex_owned/2)
 	};
 	/*
 	int matches = 0;
