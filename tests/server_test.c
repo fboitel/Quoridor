8c5f5f017cf435e54c8747fe707900705326dd91
BOITEL Faustin <faustin.boitel@bordeaux-inp.fr>
2021-05-16 17:00:28 +0200

    decrease the size of the board in server_test to have a better complexity

--- tests/server_test.c
+++ tests/server_test.c
@@ -27,7 +27,6 @@ extern char *name;
 extern size_t board_size;
 extern size_t edges;
 struct graph_t *my_board;
-struct graph_t *my_smaller_board;
 
 extern size_t position_player_1;
 extern size_t position_player_2;
@@ -44,9 +43,8 @@ extern char* (*P2_name)(void);
 
 
 static void setup(void){
-    board_size = 10;
+    board_size = 6;
     my_board = graph_init(board_size, SQUARE);
-    my_smaller_board = graph_init(6, SQUARE);
     edges = ceil(board_size * board_size * (board_size - 1) * (board_size - 1) * 2)/15;
     initialize(BLACK, my_board, edges);
   //  active_player_s = BLACK;
@@ -67,10 +65,9 @@ void fill_wall(size_t fr_1, size_t to_1, size_t fr_2, size_t to_2, struct edge_t
 
 void test_is_valid_displacement(){
     printf("%s", __func__);
-    board_size = 10;
     for (size_t i = 0; i < my_board->num_vertices - 1; i++){
         position_player_1 = i;
-        if (is_valid_displacement(my_board, -1, BLACK) || is_valid_displacement(my_board, 100, BLACK))
+        if (is_valid_displacement(my_board, -1, BLACK) || is_valid_displacement(my_board, 36, BLACK))
             FAIL("A player cannot run away from the board");
         if (is_valid_displacement(my_board, i, BLACK)){
             FAIL("A player cannot move on himself");
@@ -79,69 +76,69 @@ void test_is_valid_displacement(){
     bool error = false;
     for (int iter = 0; iter < 10; iter++)
             if (!error){
-            position_player_1 = (1 + rand()%7) + 10*(1 + rand()%7); 
-            position_player_2 = 99;
-            if (!is_valid_displacement(my_board, position_player_1 - 1, BLACK) || !is_valid_displacement(my_board, position_player_1 + 1, BLACK) || !is_valid_displacement(my_board, position_player_1 - 10, BLACK) || !is_valid_displacement(my_board, position_player_1 + 10, BLACK)){
+            position_player_1 = (1 + rand()%3) + 6*(1 + rand()%3); 
+            position_player_2 = 35;
+            if (!is_valid_displacement(my_board, position_player_1 - 1, BLACK) || !is_valid_displacement(my_board, position_player_1 + 1, BLACK) || !is_valid_displacement(my_board, position_player_1 - 6, BLACK) || !is_valid_displacement(my_board, position_player_1 + 6, BLACK)){
                 FAIL("A player should move himself in a neighbour vertex");
                 error = true;
             }
 
-            position_player_1 = (1 + rand()%7) + 10*(1 + rand()%7);    
+            position_player_1 = (1 + rand()%3) + 6*(1 + rand()%3);    
             for (size_t i = 0; i < my_board->num_vertices; i++)
-                if ((i != position_player_1 + 1 && i != position_player_1 - 1 && i != position_player_1 + 10 && i != position_player_1 - 10) && is_valid_displacement(my_board, i, BLACK)){
+                if ((i != position_player_1 + 1 && i != position_player_1 - 1 && i != position_player_1 + 6 && i != position_player_1 - 6) && is_valid_displacement(my_board, i, BLACK)){
                     FAIL("A player can't teleport himself");
                     error = true;
                 }
                 
-            position_player_1 = 10*(1 + rand()%7);
+            position_player_1 = 6*(1 + rand()%3);
             if (is_valid_displacement(my_board, position_player_1 - 1, BLACK)){
                 FAIL("A player can't cross the board");
                 error = true;
             }
 
-            position_player_1 = (1 + rand()%7) + 10*(1 + rand()%7);
+            position_player_1 = (1 + rand()%3) + 6*(1 + rand()%3);
             struct edge_t e[2];
-            fill_wall(position_player_1, position_player_1 + 10, position_player_1 + 1, position_player_1 + 11, e);
+            fill_wall(position_player_1, position_player_1 + 6, position_player_1 + 1, position_player_1 + 7, e);
             place_wall(my_board, e);
-            if (is_valid_displacement(my_board, position_player_1 + 10, BLACK)){
+            if (is_valid_displacement(my_board, position_player_1 + 6, BLACK)){
                 FAIL("A player can't jump over a wall");
                 error = true;
             }
             remove_wall(my_board, e);
 
-            position_player_1 = (1 + rand()%7) + 10*(1 + rand()%7);
+            position_player_1 = (1 + rand()%3) + 6*(1 + rand()%3);
             position_player_2 = position_player_1 - 1;
             if (is_valid_displacement(my_board, position_player_2, BLACK)){
                 FAIL("A player cannot move where another player is already");
                 error = true;
             }
                 
-            position_player_1 = (1 + rand()%7) + 10*(1 + rand()%7);
-            position_player_2 = position_player_1 + 10;
+            position_player_1 = (1 + rand()%3) + 6*(1 + rand()%3);
+            position_player_2 = position_player_1 + 6;
             if (is_valid_displacement(my_board, position_player_2, BLACK)){
                 FAIL("A player cannot move where another player is already");
                 error = true;
             }
 
-            position_player_1 = (2 + rand()%6) + 10*(2 + rand()%6);
+            position_player_1 = (1 + rand()%2) + 6*(1 + rand()%2);
             position_player_2 = position_player_1 + 1;
             if (!is_valid_displacement(my_board, position_player_2 + 1, BLACK)){
                 FAIL("A player should have the possibility to jump over another player");
                 error = true;
             }
                 
-            position_player_1 = (2 + rand()%6) + 10*(2 + rand()%6);
-            position_player_2 = position_player_1 - 10;
-            if (!is_valid_displacement(my_board, position_player_2 - 10, BLACK)){
+            position_player_1 = (2 + rand()%3) + 6*(2 + rand()%3);
+            position_player_2 = position_player_1 - 6;
+            if (!is_valid_displacement(my_board, position_player_2 - 6, BLACK)){
                 FAIL("A player should have the possibility to jump over another player");
                 error = true;
             }
             
-            position_player_1 = (2 + rand()%6) + 10*(2 + rand()%6);
-            position_player_2 = position_player_1 + 10;
-            fill_wall(position_player_2, position_player_2 + 10, position_player_2 + 1, position_player_2 + 11, e);
+            position_player_1 = (2 + rand()%2) + 6*(2 + rand()%2);
+            position_player_2 = position_player_1 + 6;
+            fill_wall(position_player_2, position_player_2 + 6, position_player_2 + 1, position_player_2 + 7, e);
             place_wall(my_board, e);
-            if (!is_valid_displacement(my_board, position_player_1 + 11, BLACK) || !is_valid_displacement(my_board, position_player_1 + 9, BLACK)){
+            if (!is_valid_displacement(my_board, position_player_1 + 7, BLACK) || !is_valid_displacement(my_board, position_player_1 + 5, BLACK)){
                 FAIL("A player should have the possibility to jump on the side of a player placed behind him if there is a wall in front of him");
                 error = true;
             }
@@ -153,48 +150,47 @@ void test_is_valid_displacement(){
 
 void test_is_valid_wall(){
     printf("%s", __func__);
-    board_size = 10;
     bool error = false;
     for (int iter = 0; iter < 10; iter++)
             if(!error){
         struct edge_t e[2];
-        size_t fr_1 = rand()%8 + 10*(rand()%8);
-        fill_wall(fr_1, fr_1 + 10, fr_1 + 1, fr_1 + 11, e);
+        size_t fr_1 = rand()%5 + 6*(rand()%5);
+        fill_wall(fr_1, fr_1 + 6, fr_1 + 1, fr_1 + 7, e);
         if (!is_valid_wall(my_board, e)) {
             FAIL("An horizontal wall can be placed anywhere in an empty board");
             error = true;
         }
 
-        fill_wall(fr_1, fr_1 + 1, fr_1 + 10, fr_1 + 11, e);
+        fill_wall(fr_1, fr_1 + 1, fr_1 + 6, fr_1 + 7, e);
         if (!is_valid_wall(my_board, e)){
             FAIL("An vertical wall can be placed anywhere in an empty board");
             error = true;
         }
 
-        fr_1 = 1 + rand()%8 + 10*(1 + rand()%8);
-        fill_wall(fr_1, fr_1 - 10, fr_1 - 1, fr_1 - 11, e);
+        fr_1 = 1 + rand()%5 + 6*(1 + rand()%5);
+        fill_wall(fr_1, fr_1 - 6, fr_1 - 1, fr_1 - 7, e);
         if (!is_valid_wall(my_board, e)){
             FAIL("The order of the vertex given to put a wall shouldn't matter");
             error = true;
         }
 
-        fr_1 = rand()%7 + 10*(rand()%7);
-        fill_wall(fr_1, fr_1 + 20, fr_1 + 1, fr_1 + 21, e);
+        fr_1 = rand()%4 + 6*(rand()%4);
+        fill_wall(fr_1, fr_1 + 12, fr_1 + 1, fr_1 + 13, e);
         if (is_valid_wall(my_board, e)){
             FAIL("The vertex separated by a wall should be connexe");
             error = true;
         }
 
 
-        fr_1 = rand()%7 + 10*(rand()%7);
-        fill_wall(fr_1, fr_1 + 10, fr_1 + 2, fr_1 + 12, e);
+        fr_1 = rand()%4 + 6*(rand()%4);
+        fill_wall(fr_1, fr_1 + 6, fr_1 + 2, fr_1 + 9, e);
         if (is_valid_wall(my_board, e)){
             FAIL("The vertex separated by a wall should be connexe");
             error = true;
         }
             
-        fr_1 = rand()%8 + 10*(rand()%8);
-        fill_wall(fr_1, fr_1 + 10, fr_1 + 1, fr_1 + 11, e);
+        fr_1 = rand()%5 + 6*(rand()%5);
+        fill_wall(fr_1, fr_1 + 6, fr_1 + 1, fr_1 + 7, e);
         place_wall(my_board, e);
         if (is_valid_wall(my_board, e)){
             FAIL("A wall cannot be placed if another one is there");
@@ -202,29 +198,29 @@ void test_is_valid_wall(){
         }
         remove_wall(my_board, e);
 
-        fr_1 = 1 + rand()%7 + 10*(1 + rand()%7);
+        fr_1 = 1 + rand()%3+ 6*(1 + rand()%3);
         struct edge_t et1[2];
-        fill_wall(fr_1, fr_1 + 10, fr_1 + 1, fr_1 + 11, et1);
+        fill_wall(fr_1, fr_1 + 6, fr_1 + 1, fr_1 + 7, et1);
         place_wall(my_board, et1);
-        fill_wall(fr_1, fr_1 + 10, fr_1 + 1, fr_1 + 11, e);
+        fill_wall(fr_1, fr_1 + 6, fr_1 + 1, fr_1 + 7, e);
         if (is_valid_wall(my_board, e)){
             FAIL("A wall cannot intercept another wall");
             error = true;
         }
         remove_wall(my_board, et1);
 
-        fr_1 = 9 + 10*(rand()%7);
-        fill_wall(fr_1, fr_1 + 10, fr_1 + 1, fr_1 + 11, e);
+        fr_1 = 5 + 6*(rand()%4);
+        fill_wall(fr_1, fr_1 + 6, fr_1 + 1, fr_1 + 7, e);
         if (is_valid_wall(my_board, e)){
             FAIL("A wall cannot begin in one side of the baord and finish in the other");
             error = true;
         }
     }
-    position_player_1 = 56;
-    struct edge_t w1[2] = {{46, 56}, {47, 57}};
-    struct edge_t w2[2] = {{57, 58}, {67, 68}};
-    struct edge_t w3[2] = {{66, 76}, {67, 77}};
-    struct edge_t w4[2] = {{55, 56}, {65, 66}};
+    position_player_1 = 21;
+    struct edge_t w1[2] = {{13, 14}, {19, 20}};
+    struct edge_t w2[2] = {{20, 26}, {21, 27}};
+    struct edge_t w3[2] = {{15, 16}, {21, 22}};
+    struct edge_t w4[2] = {{8, 14}, {9, 15}};
     place_wall(my_board, w1);
     place_wall(my_board, w2);
     place_wall(my_board, w3);
@@ -232,7 +228,7 @@ void test_is_valid_wall(){
         FAIL("A player can't be locked in by walls");
     else {
         position_player_1 = 6;
-        position_player_2 = 56;
+        position_player_2 = 21;
 
         if (is_valid_wall(my_board, w3))
             FAIL("A player can't be locked in by walls");
@@ -241,35 +237,28 @@ void test_is_valid_wall(){
     remove_wall(my_board, w2);
     remove_wall(my_board, w3);
 
-    struct edge_t bw1[2] = {{30, 40}, {31, 41}};
-    struct edge_t bw2[2] = {{32, 42}, {33, 43}};
-    struct edge_t bw3[2] = {{34, 44}, {35, 45}};
-    struct edge_t bw4[2] = {{36, 46}, {37, 47}};
-    struct edge_t bw5[2] = {{38, 48}, {39, 49}};
+    struct edge_t bw1[2] = {{6, 12}, {7, 13}};
+    struct edge_t bw2[2] = {{10, 16}, {11, 17}};
+    struct edge_t bw3[2] = {{8, 14}, {9, 15}};
     place_wall(my_board, bw1);
     place_wall(my_board, bw2);
-    place_wall(my_board, bw3);
-    place_wall(my_board, bw4);
     position_player_1 = 10;
-    position_player_2 = 15;
-    if (is_valid_wall(my_board,bw5))
+    position_player_2 = 8;
+    if (is_valid_wall(my_board,bw3))
         FAIL("A wall cannot completely block a player from the finish line");
     else {
-        position_player_1 = 80;
-    position_player_2 = 90;
-        if (is_valid_wall(my_board,bw5))
+        position_player_1 = 27;
+        position_player_2 = 28;
+        if (is_valid_wall(my_board,bw3))
             FAIL("A wall cannot completely block a player from the finish line");
     }
     remove_wall(my_board, bw1);
     remove_wall(my_board, bw2);
-    remove_wall(my_board, bw3);
-    remove_wall(my_board, bw4);
     
 }
 
 void test_update_board(){
     printf("%s", __func__);
-    board_size = 10;
     struct move_t move1 = {
         .m = 5, 
         .e = {{-1, -1}, {-1, -1}},
@@ -286,38 +275,37 @@ void test_update_board(){
     struct edge_t e[2];
     struct move_t move2 = {
         .m = 5, 
-        .e = {{5, 15}, {6, 16}},
+        .e = {{21, 27}, {22, 28}},
         .t = WALL,
         .c = BLACK
                             };
     update_board(my_board, &move2);
-    fill_wall(5, 15, 6, 16, e);
+    fill_wall(21, 27, 22, 28, e);
     if (is_valid_wall(my_board, e))
         FAIL("Update_board should put a wall if it is needed");
 }
 
 void test_is_winning(){
     printf("%s", __func__);
-    board_size = 10;
     //player_1 = WHITE, his vertices are at the top
     //player_2 = BLACK, his vertices are at the bottom
    
-    for (size_t i = 10; i < 100; i++)
+    for (size_t i = 6; i < 36; i++)
         if (is_winning(my_board, WHITE, i)){
             FAIL("player_1 should be on player_2's vertex to win");
             break;
         }
-    for (size_t i = 0; i < 90; i++)
+    for (size_t i = 0; i < 30; i++)
         if (is_winning(my_board, BLACK, i)){
             FAIL("player_2 should be on player_1's vertex to win");
             break;
         }
-    for (size_t i = 0; i < 10; i++)
+    for (size_t i = 0; i < 6; i++)
         if (!is_winning(my_board, WHITE, i)){
             FAIL("player_1 should win if he is on opponent's vertex");
             break;
         }
-    for (size_t i = 90; i < 100; i++)
+    for (size_t i = 30; i < 36; i++)
         if (!is_winning(my_board, BLACK, i)){
             FAIL("player_2 should win if he is on opponent's vertex");
             break;
@@ -326,7 +314,6 @@ void test_is_winning(){
 }
 void test_move_is_valid(){
     printf("%s", __func__);
-    board_size = 6;
     P1_lib = dlopen("build/crashboy.so", RTLD_NOW);
     P2_lib = dlopen("build/crashboy.so", RTLD_LAZY);
     P1_name = dlsym(P1_lib, "get_player_name");
@@ -337,11 +324,11 @@ void test_move_is_valid(){
     struct edge_t bw3[2] = {{20, 26}, {21, 27}};
     struct edge_t bw4[2] = {{10, 16}, {11, 17}};
     struct edge_t bw5[2] = {{16, 17}, {22, 23}};
-    place_wall(my_smaller_board, bw1);
-    place_wall(my_smaller_board, bw2);
-    place_wall(my_smaller_board, bw3);
-    place_wall(my_smaller_board, bw4);
-    place_wall(my_smaller_board, bw5);
+    place_wall(my_board, bw1);
+    place_wall(my_board, bw2);
+    place_wall(my_board, bw3);
+    place_wall(my_board, bw4);
+    place_wall(my_board, bw5);
     bool error = false;
     for (size_t pos = 0; pos < 10; pos++) {
         position_player_1 = rand()%36;
@@ -359,8 +346,8 @@ void test_move_is_valid(){
                 move.e[1].to = base_wall + 7;
                 move.t = MOVE;
                 move.c = rand()%2;
-                bool val_disp = is_valid_displacement(my_smaller_board, move.m, move.c);
-                bool mov_val = move_is_valid(&move, my_smaller_board, move.c);
+                bool val_disp = is_valid_displacement(my_board, move.m, move.c);
+                bool mov_val = move_is_valid(&move, my_board, move.c);
                 if (val_disp && !mov_val){
                     FAIL("!move_is_valid but found is_valid_displacement");
                     error = true;
@@ -371,8 +358,8 @@ void test_move_is_valid(){
                 }
 
                 move.t = WALL;
-                bool val_wall = is_valid_wall(my_smaller_board, move.e);
-                mov_val = move_is_valid(&move, my_smaller_board, move.c);
+                bool val_wall = is_valid_wall(my_board, move.e);
+                mov_val = move_is_valid(&move, my_board, move.c);
                 if (val_wall && !mov_val){
                     FAIL("!move_is_valid but found is_valid_wall");
                     error = true;
@@ -383,11 +370,11 @@ void test_move_is_valid(){
                 }
             }
     }
-    remove_wall(my_smaller_board, bw1);
-    remove_wall(my_smaller_board, bw2);
-    remove_wall(my_smaller_board, bw3);
-    remove_wall(my_smaller_board, bw4);
-    remove_wall(my_smaller_board, bw5);
+    remove_wall(my_board, bw1);
+    remove_wall(my_board, bw2);
+    remove_wall(my_board, bw3);
+    remove_wall(my_board, bw4);
+    remove_wall(my_board, bw5);
     dlclose(P1_lib);
     dlclose(P2_lib);
     freopen("/dev/tty","r",stderr);
